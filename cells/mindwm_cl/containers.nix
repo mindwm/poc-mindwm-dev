4d95eedb84e2d2a172bd607e8f770e4c0e6abd0e
Sergey Bubnov <omgbebebe@gmail.com>
2024-03-07 13:58:48 +0400

    fix containers definition

--- cells/mindwm_cl/containers.nix
+++ cells/mindwm_cl/containers.nix
@@ -8,14 +8,21 @@
 in {
   vector_back = std.lib.ops.mkStandardOCI rec {
     operable = cell.apps.vector_back;
-    name = "vector";
+    name = "vector-back";
+    tag = "latest";
+    inherit (operable) meta;
+#    meta.mainProgram = "${operable.package}/out/vector";
+  };
+  vector_client = std.lib.ops.mkStandardOCI rec {
+    operable = cell.apps.vector_client;
+    name = "vector-client";
     tag = "latest";
     inherit (operable) meta;
 #    meta.mainProgram = "${operable.package}/out/vector";
   };
   nats_back = std.lib.ops.mkStandardOCI rec {
     operable = cell.apps.nats_back;
-    name = "nats";
+    name = "nats-back";
     tag = "latest";
     inherit (operable) meta;
   };
