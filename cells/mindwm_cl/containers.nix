72cfa21c42f18f8cbc10618a11eda79ad0ed0a56
Sergey Bubnov <omgbebebe@gmail.com>
2024-03-07 14:11:50 +0400

    prefix OCI images with organization name

--- cells/mindwm_cl/containers.nix
+++ cells/mindwm_cl/containers.nix
@@ -8,21 +8,21 @@
 in {
   vector_back = std.lib.ops.mkStandardOCI rec {
     operable = cell.apps.vector_back;
-    name = "vector-back";
+    name = "mindwm/vector-back";
     tag = "latest";
     inherit (operable) meta;
 #    meta.mainProgram = "${operable.package}/out/vector";
   };
   vector_client = std.lib.ops.mkStandardOCI rec {
     operable = cell.apps.vector_client;
-    name = "vector-client";
+    name = "mindwm/vector-client";
     tag = "latest";
     inherit (operable) meta;
 #    meta.mainProgram = "${operable.package}/out/vector";
   };
   nats_back = std.lib.ops.mkStandardOCI rec {
     operable = cell.apps.nats_back;
-    name = "nats-back";
+    name = "mindwm/nats-back";
     tag = "latest";
     inherit (operable) meta;
   };
