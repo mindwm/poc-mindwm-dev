37ced5c498b7a261b97aa91afac98fa0854dcf09
Sergey Bubnov <omgbebebe@gmail.com>
2024-03-14 12:54:50 +0400

    do not change cwd in tmux panes

--- cells/mindwm_cl/scripts/client_inside_tmux.sh
+++ cells/mindwm_cl/scripts/client_inside_tmux.sh
@@ -10,6 +10,6 @@ export MINDWM_CLIENT_NATS_SUBJECT_FEEDBACK="$(operable-current-subject).feedback
 
 printf "Backend NATS subject: %s" "${MINDWM_BACK_NATS_SUBJECT_WORDS_IN}\n"
 
-pushd ./mindwm-manager/src >/dev/null 2>&1
-python3 ./main.py
-popd >/dev/null 2>&1
+#pushd ./mindwm-manager/src >/dev/null 2>&1
+python3 ./mindwm-manager/src/main.py
+#popd >/dev/null 2>&1
